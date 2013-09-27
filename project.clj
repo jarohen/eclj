f70f14441c3fbac0d4de7a847da7f861a3ea4f78
James Henderson <james@pcr.me.uk>
2013-09-27 21:43:33 +0100

    Initial commit

---
+++ project.clj
@@ -0,0 +1,6 @@
+(defproject jarohen/eclj "0.1.0-SNAPSHOT"
+  :description "A Clojure library to execute Clojure forms embedded inside text files - similar to ERB."
+  :url "https://github.com/james-henderson/eclj"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.5.1"]])
